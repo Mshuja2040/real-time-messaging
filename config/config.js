696b85379889bfebb8283bba8e1c5b57cd6965db
Mshuja2040 <muhammadshuja2040@gmail.com>
2024-11-20 17:49:24 +0500

    changes befroe deploy

--- config/config.js
+++ config/config.js
@@ -6,6 +6,7 @@ export default {
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: process.env.DB_DIALECT || 'mysql',
     logging: console.log, // Enable logging for development (or customize as needed)
   },
